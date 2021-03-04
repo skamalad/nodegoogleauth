1d85ed43bc4af66818850da18206b0e1b7066293
Sachin Kamaladharan <skamalad@Sachins-MacBook-Pro.local>
2021-03-04 19:12:54 +0530

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+// @desc Authenticate with Goole
+// @route GET/auth/google
+
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+
+// @desc Google Auth callback
+// @route GET /auth/google/callback
+// router.get('/google/callback', 
+//     passport.authenticate('google', {
+//         successRedirect: '/dashboard',
+//         failureRedirect: '/'
+//     }))
+
+router.get ('/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/' }), 
+    ( req, res ) => {
+        res.redirect('/dashboard')
+    })
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const express = require("express")
+const dotenv = require("dotenv")
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const connectDB = require('./config/db')
+
+
+// Load config
+dotenv.config( { path: './config/config.env' } )
+connectDB()
+
+// Passport Config
+require('./config/passport')(passport)
+
+
+const app = express()
+// Logging
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan( 'dev' ))
+}
+
+// Handlebars
+app.engine( '.hbs', exphbs( { defaultLayout: 'main', extname: 'hbs' } ) );
+app.set('view engine', '.hbs'); 
+
+// Sessions Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+    // cookie: { secure: true }
+  }))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static Folder
+app.use( express.static( path.join( __dirname, 'public' ) ) )
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
